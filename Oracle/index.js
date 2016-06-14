26c7374a17fa6ee75a9d85aad1a82539c5e247e0
Andy <aliko.andy@gmail.com>
2016-06-14 12:57:33 +0200

    changed the parsing

--- Oracle/index.js
+++ Oracle/index.js
@@ -6,9 +6,13 @@ var PythonShell = require('python-shell');
 var Web3 = require('web3');
 //end Imports
 var web3 = new Web3();
-var pyshell =  new PythonShell();
-
-localhost ="http://localhost:8545";
+// var pyshell =  new PythonShell();
+// var reader = require ("buffered-reader");
+// var BinaryReader = reader.BinaryReader;
+// var DataReader = reader.DataReader;
+var natural = require('natural'),
+tokenizer = new natural.WordTokenizer();
+localhost ="http://127.0.0.1:8545";
 external= '' // PASTE EXTERNAL RPC PROVIDER HERE, e.g. Morden
 
 
@@ -33,25 +37,31 @@ console.log(accounts); // return all the accounts managed by the node
 
 //warning: THIS IS A TEST CONTRACT THAT ONLY RESIDES ON MY PRIVATE CHAIN
 //to test your own contracts, you need to paste the ABI of your own contract and the change the contract address below as well as the called methods in myConstractInstance.
+//
+// abi = [{"constant":false,"inputs":[],"name":"kill","outputs":[],"type":"function"},{"constant":true,"inputs":[],"name":"greet","outputs":[{"name":"","type":"string"}],"type":"function"},{"inputs":[{"name":"_greeting","type":"string"}],"type":"constructor"}]
+// ;
+//
+//
+// // creation of contract object
+// var MyContract = web3.eth.contract(abi);
+//
+// // initiate contract for an address
+// var myContractInstance = MyContract.at('0x30df1ca827f54a3bc213b97e69e6386737fc0a26');
+//
+// var result = myContractInstance.greet();
+// console.log(result);
+// //end warning
+var file = "mobydick.txt";
+var lineReader = require('readline').createInterface({
+  input: require('fs').createReadStream(file)
+});
 
-abi = [{"constant":false,"inputs":[],"name":"kill","outputs":[],"type":"function"},{"constant":true,"inputs":[],"name":"greet","outputs":[{"name":"","type":"string"}],"type":"function"},{"inputs":[{"name":"_greeting","type":"string"}],"type":"constructor"}];
-
-
-// creation of contract object
-var MyContract = web3.eth.contract(abi);
-
-// initiate contract for an address
-var myContractInstance = MyContract.at('0x0067ef605fd5bc0150eee5c990990ef9921e72cc');
-
-var result = myContractInstance.greet();
-console.log(result);
-//end warning
-
-
-
-//TODO: Implement the main loop:
-      //TODO: write methods that read the book and send words to contracts
-      // words must only be read once the system has ticked
-
-
-      //TODO: Import Python-shell plugin, figure out how python can be called from within nodejs,
+lineReader.on('line', function (line) {
+  // console.log('Line from file:', line);
+  //TODO: read information and send information to contracts
+  //TODO: Wait for TICK to occur, then retrieve graph curves from letter factories
+  //TODO: retrieve graph curves and what not, send the information to python-shell
+  //TODO: based on what python-shell returned call the contracts and set some variables there.
+  var words = tokenizer.tokenize(line);
+  console.log(words);
+});
