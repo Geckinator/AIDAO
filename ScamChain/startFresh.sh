bfcae06111e63ddafd48b4e88194fe95fedf2f0b
Andy <aliko.andy@gmail.com>
2016-06-20 08:24:36 +0200

    removed

--- ScamChain/startFresh.sh
+++
@@ -1,1 +0,0 @@
-geth --genesis genesis.json --datadir data --networkid 13 --nodiscover --maxpeers 1 console
--- ScamChain/theChain.sh
+++
@@ -1,1 +0,0 @@
-geth --ipcpath "/home/kingkongkai/.ethereum/geth.ipc" --datadir data --nodiscover --rpc --rpcaddr "localhost" --rpcport "8545" --networkid 13 --maxpeers 2 console
